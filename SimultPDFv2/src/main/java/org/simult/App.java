95411c5ceb68eee5e2691e456eb0dc54f6be06a8
Leonardo_Dantas <l.dantas1155@gmail.com>
2023-09-04 19:48:35 -0300

    Adicionando uma dependência para criptografar as senhas no arquivo pom.xml e criando o menu na APP.

--- SimultPDFv2/src/main/java/org/simult/App.java
+++ SimultPDFv2/src/main/java/org/simult/App.java
@@ -1,11 +1,17 @@
 package org.simult;
 
+
 import java.sql.*;
 import org.simult.models.bd.Conexao;
 import org.simult.models.entity.Relatorio;
 
+import org.mindrot.jbcrypt.BCrypt;
 import java.sql.Connection;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.simult.controllers.AdministradorController.logar;
 
 /**
  * Hello world!
@@ -14,31 +20,47 @@ import java.util.Scanner;
 public class App  {
     public static void main( String[] args ) {
         Scanner input = new Scanner(System.in);
-        /*
-        Connection conex = Conexao.getInstance().getConexao();
 
-        if(conex != null){
-            System.out.println("yesss\n");
+        String login = "";
+        String senha = "";
+
+        int tentativas = 0;
+        int opcao = 0;
+        boolean acesso = false;
+
+        System.out.println("\n\t\t\t **** SIMULT-PDF  V.2 **** \n\n");
+
+        do {
+            // Entrada do valor do login do administrador
+            System.out.println("\t- Login: \n");
+            login = input.nextLine();
+
+            // Entrada do valor da senha do administrador
+            System.out.println("\t- Senha: \n");
+            senha = input.nextLine();
+
+            senha = BCrypt.hashpw(senha, BCrypt.gensalt());
+
+            if (!logar(login, senha)){
+                System.out.println("\n\t\t Credenciais incorretas! \n\n");
+                tentativas++;
+            } else {
+                System.out.println("\n\t\t Logando no sistema! \n\n");
+                acesso = true;
+            }
+
+        } while (tentativas < 3 && !acesso);
+
+        if(tentativas == 3){
+            System.out.println("\n\t\t Acesso bloqueado!!!\n\n");
         } else {
-            System.out.println("noooo\n");
+            try {
+
+            } catch (){
+
+            }
+
+            switch ()
         }
-         */
-
-        String placa = "";
-
-        System.out.println("\t- Escreva a placa do veículo: \n\t");
-        placa = input.nextLine();
-
-        System.out.println(placa.substring(0, 3).matches("[a-z][A-Z]*"));
-
-/*
-        if(!(placa.substring(0, 3).contains("^[a-Z]")) && !(placa.substring(3).contains("[^0-9]")) && !(placa.substring(4).contains("[^a-Z]")) && !(placa.substring(5, 7).matches("^[0-9]+$"))){       //Character.isDigit(placa.charAt(i));   matches("[0-9]+");
-            System.out.println("\t\t\n Formato da placa do veículo correto!\n");
-        } else {
-            System.out.println("\t\t\n Formato da placa do veículo incorreta!\n");
-        }
-
- */
-
     }
 }
