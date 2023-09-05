f48b20eb3957d05d305c6622093dd18d5777ef12
Leonardo_Dantas <l.dantas1155@gmail.com>
2023-09-05 09:16:04 -0300

    Criando e corrigindo alguns detalhes do menu da aplicação.

--- SimultPDFv2/src/main/java/org/simult/App.java
+++ SimultPDFv2/src/main/java/org/simult/App.java
@@ -1,66 +1,74 @@
 package org.simult;
 
+import org.simult.models.entity.Administrador;
 
-import java.sql.*;
-import org.simult.models.bd.Conexao;
-import org.simult.models.entity.Relatorio;
-
-import org.mindrot.jbcrypt.BCrypt;
-import java.sql.Connection;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static org.simult.controllers.AdministradorController.logar;
+import static org.simult.controllers.AdministradorController.*;
+import static org.simult.models.dao.AdministradorDAO.buscaAdministrador;
 
-/**
- * Hello world!
- *
- */
 public class App  {
     public static void main( String[] args ) {
         Scanner input = new Scanner(System.in);
+        Administrador adm = null;
 
-        String login = "";
-        String senha = "";
 
-        int tentativas = 0;
         int opcao = 0;
-        boolean acesso = false;
 
         System.out.println("\n\t\t\t **** SIMULT-PDF  V.2 **** \n\n");
 
+        adm = logar();
+
         do {
-            // Entrada do valor do login do administrador
-            System.out.println("\t- Login: \n");
-            login = input.nextLine();
+            if (adm != null) {
+                System.out.println("\n\t- Que operação deseja realizar:");
+                System.out.print("\t\t1. Cadastrar novo administrador\n\t\t2. Buscar veículo\n\t\t3. Buscar multa\n\t\t4. Vincular multa a um veículo\n\t\t5. Desvincular multa de um veículo\n\t\t6. Gerar relatório de multas\n\t\t7. Gerar relatório de ações gerais\n\t\t8. Sair\n\t\t9. Fechar sistema\n\tOpcao: ");
+                try {
+                    opcao = Integer.parseInt(input.nextLine());
 
-            // Entrada do valor da senha do administrador
-            System.out.println("\t- Senha: \n");
-            senha = input.nextLine();
-
-            senha = BCrypt.hashpw(senha, BCrypt.gensalt());
-
-            if (!logar(login, senha)){
-                System.out.println("\n\t\t Credenciais incorretas! \n\n");
-                tentativas++;
+                    switch (opcao) {
+                        case 1:
+                            if (cadastrarAdministrador(adm)) {
+                                System.out.println("\n\t\t Cadastramento concluído com sucesso!");
+                            } else if (!cadastrarAdministrador(adm)) {
+                                System.out.println("\n\t\t Falha no cadastramento!");
+                            }
+                            break;
+                        case 2:
+                            consultarVeiculo();
+                            break;
+                        case 3:
+                            consultarMulta();
+                            break;
+                        case 4:
+                            vincularMulta(adm);
+                            break;
+                        case 5:
+                            desvincularMulta(adm);
+                            break;
+                        case 6:
+                            //gerarRelatorio(adm);
+                            break;
+                        case 7:
+                            //gerarRelatorio(adm);
+                            break;
+                        case 8:
+                            adm = null;
+                            break;
+                        case 9:
+                            System.out.println("\n\t\t Encerrando sistema!!!\n");
+                            break;
+                        default:
+                            System.out.println("\n\t\t Opção inválida!!!");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("\n\t\t O campo opção aceita somente números!\n");
+                }
             } else {
-                System.out.println("\n\t\t Logando no sistema! \n\n");
-                acesso = true;
+                System.out.println("\n\t\t Para acessar o sistema é necessário está logado!\n");
+                adm = logar();
             }
-
-        } while (tentativas < 3 && !acesso);
-
-        if(tentativas == 3){
-            System.out.println("\n\t\t Acesso bloqueado!!!\n\n");
-        } else {
-            try {
-
-            } catch (){
-
-            }
-
-            switch ()
-        }
+        } while (opcao != 9);
     }
 }
